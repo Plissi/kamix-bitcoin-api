362b616173104e8da156571f15b5045261ef62a4
Plissi <lockflore@gmail.com>
2021-02-04 09:47:20 +0100

    remove hostname

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var http = require('http');
-const { hostname } = require('os');
 var app = require('./app')
 
-const hotname ='127.0.0.1'
+const hostname ='127.0.0.1'
 const port = process.env.PORT || 3000;
 app.set('port', port)
 
 const server = http.createServer(app)
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })

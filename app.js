20e0a3d1877df431a59dee1a62d668311c0b0e94
Plissi <lockflore@gmail.com>
2021-03-12 06:57:05 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const bodyParser = require("body-parser");
 const rpcMethods = require("./routes/api");
 var helmet = require("helmet")
@@ -6,15 +6,36 @@ var helmet = require("helmet")
 const app = express();
 
 app.use(helmet())
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use("/api", rpcMethods);
+
+module.exports = app*/
+
+ /*
+  * NEW
+  */
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var helmet = require("helmet");
+const rpcMethods = require("./routes/api");
+const app = express();
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use(helmet())
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use("/api", rpcMethods);
 
-module.exports = app
+module.exports = app;

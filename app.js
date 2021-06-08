f2b127f510b74826dba35cb55fb4dfdf003c2ea4
Plissi <lockflore@gmail.com>
2021-06-08 14:20:46 +0100

    updating documentation

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCrea
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, x-access-token, Host');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "KAMIX Bitcoin API",
-  "version": "0.3.0",
+  "version": "1.0.0",
   "description": "Documentation of the KAMIX Bitcoin API",
   "apidoc": "0.3.0",
   "title": "Custom apiDoc browser title",
@@ -9,7 +9,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-06T19:49:11.494Z",
+    "time": "2021-06-06T20:02:04.599Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }

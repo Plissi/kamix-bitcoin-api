c841cf7a1440e5c41212de1249333c70e9125b8c
Plissi <lockflore@gmail.com>
2021-06-10 19:46:46 +0100

    modified history bitcoin price fetching

--- functions/past_bitcoin_price.js
+++ functions/past_bitcoin_price.js
@@ -13,16 +13,17 @@ const options = {
 
 exports.pastBtcPrice = (date)=>{
     return new Promise(resolve=>{
-        if (date < new Date(2013, 4, 28).getTime()) {
+        if (date < new Date(2010, 7, 18).getTime()) {
+            /*
             date = new Date(date);
             let day, month, year;
             year = date.getFullYear();
             month = date.getMonth()+1;
             day = date.getDate();
-            
+
             if (month <10) month = '0'+month;
             if (day <10) day = '0'+day;
-        
+
             let date_string = year+'-'+month+'-'+day
             let url = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${date_string}&end=${date_string}&currency=eur`
             var httpsRequest = https.request(url, options, (response)=>{
@@ -40,25 +41,27 @@ exports.pastBtcPrice = (date)=>{
                     }
                 })
             })
-    
+
             httpsRequest.on('error', function(e) {
-                console.log('problem with price request: ' + e.message);   
+                console.log('problem with price request: ' + e.message);
                 fs.appendFile('logs/error.log', e.toString()+'\n', 'utf8', (error) => {
                     if (error) throw error;
                 });
               });
-        
-            httpsRequest.end()
+
+            httpsRequest.end()*/
+            resolve(0.0858)
         } else{
+            resolve()
             date = new Date(date);
             let day, month, year;
             year = date.getFullYear();
             month = date.getMonth()+1;
             day = date.getDate();
-            
+
             if (month <10) month = '0'+month;
             if (day <10) day = '0'+day;
-        
+
             let date_string = day+'-'+month+'-'+year
 
             CoinGeckoClient.coins.fetchHistory('bitcoin', {
@@ -67,7 +70,7 @@ exports.pastBtcPrice = (date)=>{
             }).then(data =>{
                 resolve(data.data.market_data.current_price.eur)
             }).catch(error => {
-                console.log('problem with price request: ' + error.message);  
+                console.log('problem with price request: ' + error.message);
             })
         }
     })
